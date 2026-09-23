a26ff5d371e9b7625b68715b8150feab21616e20
Dev <dev@Victors-MacBook-Pro-2.local>
2026-09-23 10:27:35 +0200

    fixing the issue

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask inside Docker!"
+    return "Hello from Flask inside Docker Dalton!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
